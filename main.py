0a09dc048609e6e703cbdf077008e074d75f9fe1
Daniel Wadell <dwadell@csumb.edu>
2018-08-07 18:24:29 +0000

    movie api

--- main.py
+++ main.py
@@ -14,10 +14,7 @@ the_jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
     
-def run_query(first_line, second_line, pic_type):
-    movie_choices = Movie(line1=first_line, line2 = second_line, img_choice = pic_type)
-    movie_key = movie_choices.put()
-    print movie_key
+
 
     
 class Welcome(webapp2.RequestHandler):
@@ -31,15 +28,21 @@ class Results(webapp2.RequestHandler):
     def get(self):
         about_template = the_jinja_env.get_template('templates/results.html')
         self.response.write(about_template.render())
-    
-class MovieHandler(webapp2.RequestHandler):
-    def get(self): 
         results_template = the_jinja_env.get_template('templates/results.html')
         comedy_choice = self.request.get('user-comedy-choice')
         mystery_choice = self.request.get('user-mystery-choice')
         action_choice = self.request.get('user-action-choice')
         drama_choice = self.request.get('user-drama-choice')
         horror_choice = self.request.get('user-horror-choice')
+        
+        movie_dict = {
+            'action': 'https://www.youtube.com/watch?v=gKTVQPOH8ZA',
+            
+        }
+        
+        
+
+
     
 
 app = webapp2.WSGIApplication([
