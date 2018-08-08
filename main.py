844cb879a7945a932ebf62c19e67b3331411b41f
Daniel Wadell <dwadell@csumb.edu>
2018-08-08 23:59:58 +0000

    heuheuheu

--- main.py
+++ main.py
@@ -25,49 +25,73 @@ class Welcome(webapp2.RequestHandler):
    
 class Comedy(webapp2.RequestHandler):
     def get(self):
+        spot = randint(0,4)
         about_template = the_jinja_env.get_template('templates/comedy.html')
         comedy_movies = ['https://www.youtube.com/embed/cd-go0oBF4Y','https://www.youtube.com/embed/CXqxP-bUC7I',
         'https://www.youtube.com/embed/l13yPhimE3o','https://www.youtube.com/embed/D6gABQFR94U','https://www.youtube.com/embed/MNpoTxeydiY']
-        shuffle(comedy_movies)
-        movie_pick = comedy_movies[0]
+        comedy_movies_title = ['Shutter Island','Inception','Memento','Zodiac','Se7en']
+        comedy_movies_director = ['Martin Scorsese', 'Christopher Nolan', 'Christopher Nolan', 'David Fincher', 'David Fincher']
+        movie_pick = comedy_movies[spot]
+        movie_title_pick = comedy_movies_title[spot]
+        movie_director = comedy_movies_director[spot]
         comedy_dict = {
-            'movie_trailer': movie_pick
+            'movie_trailer': movie_pick,
+            'movie_title': movie_title_pick,
+            'movie_director': movie_director
         }
         self.response.write(about_template.render(comedy_dict))
     
 class Drama(webapp2.RequestHandler):
     def get(self):
+        spot = randint(0,4)
         about_template = the_jinja_env.get_template('templates/drama.html')
         drama_movies = ['https://www.youtube.com/embed/lB95KLmpLR4','https://www.youtube.com/embed/5gcyB72nFmc',
         'https://www.youtube.com/embed/89Kq8SDyvfg','https://www.youtube.com/embed/6hB3S9bIaco','https://www.youtube.com/embed/M5FpB6qDGAE']
-        shuffle(drama_movies)
-        movie_pick = drama_movies[0]
+        drama_movies_title = ['Shutter Island','Inception','Memento','Zodiac','Se7en']
+        drama_movies_director = ['Martin Scorsese', 'Christopher Nolan', 'Christopher Nolan', 'David Fincher', 'David Fincher']
+        movie_pick = drama_movies[spot]
+        movie_title_pick = drama_movies_title[spot]
+        movie_director = drama_movies_director[spot]
         drama_dict = {
-            'movie_trailer': movie_pick
+            'movie_trailer': movie_pick,
+            'movie_title': movie_title_pick,
+            'movie_director': movie_director
         }
         self.response.write(about_template.render(drama_dict))
 
 class Horror(webapp2.RequestHandler):
     def get(self):
+        spot = randint(0,4)
         about_template = the_jinja_env.get_template('templates/horror.html')
         horror_movies = ['https://www.youtube.com/embed/k10ETZ41q5o','https://www.youtube.com/embed/k5WQZzDRVtw',
         'https://www.youtube.com/embed/F_UxLEqd074','https://www.youtube.com/embed/a_Hw4bAUj8A','https://www.youtube.com/embed/lQKs169Sl0I']
-        shuffle(horror_movies)
-        movie_pick = horror_movies[0]
+        horror_movies_title = ['Shutter Island','Inception','Memento','Zodiac','Se7en']
+        horror_movies_director = ['Martin Scorsese', 'Christopher Nolan', 'Christopher Nolan', 'David Fincher', 'David Fincher']
+        movie_pick = horror_movies[spot]
+        movie_title_pick = horror_movies_title[spot]
+        movie_director = horror_movies_director[spot]
         horror_dict = {
-            'movie_trailer': movie_pick
+            'movie_trailer': movie_pick,
+            'movie_title': movie_title_pick,
+            'movie_director': movie_director
         }
         self.response.write(about_template.render(horror_dict))
 
 class Action(webapp2.RequestHandler):
     def get(self):
+        spot = randint(0,4)
         about_template = the_jinja_env.get_template('templates/action.html')
         action_movies = ['https://www.youtube.com/embed/bvaftiAu7mw','https://www.youtube.com/embed/gKTVQPOH8ZA',
         'https://www.youtube.com/embed/pbA-tBrHNfI','https://www.youtube.com/embed/c4Jo8QoOTQ4','https://www.youtube.com/embed/0ZOcoxjeUYo']
-        shuffle(action_movies)
-        movie_pick = action_movies[0]
+        action_movies_title = ['John Wick','Training Day','Taken','The Terminator','Raiders of the Lost Ark']
+        action_movies_director = ['Chad Stahelski', 'Antoine Fuqua', 'Pierre Morel', 'James Cameron', 'Steven Spielberg']
+        movie_pick = action_movies[spot]
+        movie_title_pick = action_movies_title[spot]
+        movie_director = action_movies_director[spot]
         action_dict = {
-            'movie_trailer': movie_pick
+            'movie_trailer': movie_pick,
+            'movie_title': movie_title_pick,
+            'movie_director': movie_director
         }
         self.response.write(about_template.render(action_dict))
     
