915259fcc74eabcd4d8db8dc8482afd0263bface
Daniel Wadell <dwadell@csumb.edu>
2018-08-09 17:12:04 +0000

    big update

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ class Comedy(webapp2.RequestHandler):
         about_template = the_jinja_env.get_template('templates/comedy.html')
         comedy_movies = ['https://www.youtube.com/embed/cd-go0oBF4Y','https://www.youtube.com/embed/CXqxP-bUC7I',
         'https://www.youtube.com/embed/l13yPhimE3o','https://www.youtube.com/embed/D6gABQFR94U','https://www.youtube.com/embed/MNpoTxeydiY']
-        comedy_movies_title = ['Shutter Island','Inception','Memento','Zodiac','Se7en']
-        comedy_movies_director = ['Martin Scorsese', 'Christopher Nolan', 'Christopher Nolan', 'David Fincher', 'David Fincher']
+        comedy_movies_title = ['The Big Lebowski','Meet the Parents','Dumb and Dumber','Ferris Buellers Day Off','SuperBad']
+        comedy_movies_director = ['Ethan and Joel Cohen', 'Jay Roach', 'Peter Farrelly', 'John Hughes', 'Greg Mottola']
         movie_pick = comedy_movies[spot]
         movie_title_pick = comedy_movies_title[spot]
         movie_director = comedy_movies_director[spot]
@@ -47,8 +47,8 @@ class Drama(webapp2.RequestHandler):
         about_template = the_jinja_env.get_template('templates/drama.html')
         drama_movies = ['https://www.youtube.com/embed/lB95KLmpLR4','https://www.youtube.com/embed/5gcyB72nFmc',
         'https://www.youtube.com/embed/89Kq8SDyvfg','https://www.youtube.com/embed/6hB3S9bIaco','https://www.youtube.com/embed/M5FpB6qDGAE']
-        drama_movies_title = ['Shutter Island','Inception','Memento','Zodiac','Se7en']
-        drama_movies_director = ['Martin Scorsese', 'Christopher Nolan', 'Christopher Nolan', 'David Fincher', 'David Fincher']
+        drama_movies_title = ['The Social Network','The Imitation Game','The Pursuit of Happyness','The Shawshank Redemption','Schindlers List']
+        drama_movies_director = ['David Fincher', 'Morten Tyldum', 'Gabriele Muccino', 'Frank Darabont', 'Steven Spielberg']
         movie_pick = drama_movies[spot]
         movie_title_pick = drama_movies_title[spot]
         movie_director = drama_movies_director[spot]
@@ -65,8 +65,8 @@ class Horror(webapp2.RequestHandler):
         about_template = the_jinja_env.get_template('templates/horror.html')
         horror_movies = ['https://www.youtube.com/embed/k10ETZ41q5o','https://www.youtube.com/embed/k5WQZzDRVtw',
         'https://www.youtube.com/embed/F_UxLEqd074','https://www.youtube.com/embed/a_Hw4bAUj8A','https://www.youtube.com/embed/lQKs169Sl0I']
-        horror_movies_title = ['Shutter Island','Inception','Memento','Zodiac','Se7en']
-        horror_movies_director = ['Martin Scorsese', 'Christopher Nolan', 'Christopher Nolan', 'David Fincher', 'David Fincher']
+        horror_movies_title = ['The Conjuring','The Babadook','Paranormal Activity','The Blair Witch Project','The Silence of the Lambs']
+        horror_movies_director = ['James Wan', 'Jennifer Kent', 'Oren Peli', 'Eduardo Sánchez', 'Jonathan Demme']
         movie_pick = horror_movies[spot]
         movie_title_pick = horror_movies_title[spot]
         movie_director = horror_movies_director[spot]
